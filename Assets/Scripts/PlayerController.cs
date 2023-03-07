ea8eff835a7fbf2032ae84887cb51b2779919b50
Raymond Edbert Karnady <100107806+RayED021@users.noreply.github.com>
2023-03-07 12:27:57 +0700

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +12,10 @@ public class PlayerController : MonoBehaviour
     public float direction;
     public float jumpForce = 300f;
     public SpriteRenderer sprite;
+    public Collider2D coll;
+    public bool isGrounded;
+    public RawImage img;
+    public LoadScene load;
 
  
     void Start()
@@ -17,6 +23,7 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        img.gameObject.SetActive(false);
     }
 
     void Update()
@@ -36,20 +43,64 @@ public class PlayerController : MonoBehaviour
         {
             sprite.flipX = false;
         }
-  
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+
+        /*if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Attack();
-        }
+        }*/
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //if(coll.IsTouching())
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                rb.AddForce(transform.up * jumpForce);
+            }
+
+        //InvokeVideo();
+
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            rb.AddForce(transform.up * jumpForce);
+            isGrounded = true;
         }
-    }   
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
     public void Attack()
     {
         animator.SetTrigger("isAttack");
+    }
+
+    public void InvokeVideo()
+    {
+        Invoke("PlayVideo", 2f);
+    }
+
+    public void PlayVideo()
+    {
+        img.gameObject.SetActive(true);
+    }
+
+    public void ChangeScene()
+    {
+        StartCoroutine("Scene");
+    }
+
+    public IEnumerator Scene()
+    {
+        yield return new WaitForSeconds(3f);
+        load.ChangeMyScene("Menu");
     }
 
 }
